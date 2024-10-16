92e4caf0150e0f2a380fcef3e536d0a4442981bf
ShayanBhatti <shayanbhatti332@gmail.com>
2024-10-16 16:57:34 +0500

    boss

--- server.js
+++ server.js
@@ -11,34 +11,16 @@ const someOtherPlaintextPassword = 'pass123';
  
 
 
-// async function hashcompare(myPlaintextPassword,saltRounds){
-//  const hashs =  await bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-//         console.log(hash);
-// const  hashscompare = await bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-//             console.log(res);
-//           });
-//         });
-// }
 
-async function hashAndComparePassword(myPlaintextPassword, saltRounds) {
-    try {
-      // Hash the password asynchronously
-      const hashedPassword = await bcrypt.hash(myPlaintextPassword, saltRounds);
-  
-      // Simulate user input (replace with actual user input)
-      const userProvidedPassword = 'myPlaintextPassword'; // Replace with actual input
-  
-      // Compare the hashed password with the user-provided password asynchronously
-      const match = await bcrypt.compare(userProvidedPassword, hashedPassword);
-  
-      console.log('Hashed password:', hashedPassword); // Remove this line in production
-      console.log('Password match:', match);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  }
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword,saltRounds, (err,hash)=>{
+    console.log(hash);
+    bcrypt.compare(someOtherPlaintextPassword, hash, (err,res)=>{
+        console.log(res);
+        
+    })
+    
+})
 
 
 //END_ASYNC
