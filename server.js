052f599748373065b296c25a17c6ccdc32386e64
ShayanBhatti <shayanbhatti332@gmail.com>
2024-10-16 17:05:10 +0500

    The end

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ bcrypt.hash(myPlaintextPassword,saltRounds, (err,hash)=>{
 
 //START_SYNC
 
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
